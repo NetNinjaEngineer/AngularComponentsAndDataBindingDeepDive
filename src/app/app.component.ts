d5453a0536e00d801457ac98938a52a41114359b
NetNinjaEngineer <memo5260287@outlook.com>
2024-04-01 15:35:37 +0200

    AngularComponentsAndDataBindingDeepDive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ComponentAndDataBindingDeepDive';
+  servers: any[] = [];
+  newServerName: string = '';
+  newServerContent: string = '';
+
+  onAddServer(){
+    this.servers.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    })
+  }
+
+  onAddBlueprint(){
+    this.servers.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    })
+  }
+
+
 }
